497f3089e3d5bfca42cbd5988f15f59698f7e15a
ayman <aymanmohamed1142001@gmail.com>
2026-08-24 22:23:59 +0300

    create cubits

---
+++ lib/cubits/get_Weather_cuibts/get_weather_cuibit.dart
@@ -0,0 +1,17 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:weather_app/cubits/get_Weather_cuibts/get_weather_states.dart';
+
+class GetWeatherCuibit extends Cubit<WeatherStates> {
+ GetWeatherCuibit():super(InitialState());
+ 
+
+ 
+  }
+
+
+
+
+
+
+
